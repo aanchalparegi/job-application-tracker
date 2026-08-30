1ad633139f9b6d52344abad883360830d2de98c8
Aanchal Paregi <aanchalparegi@mac.home>
2026-08-30 18:47:41 -0400

    Initial React project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <h1>Job Application Tracker</h1>
+      <p>Track your job applications in one place.</p>
+
+      <button>Add Application</button>
+    </div>
+  );
+}
+
+export default App;
